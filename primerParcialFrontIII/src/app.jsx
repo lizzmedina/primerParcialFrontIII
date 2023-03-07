c17f5d128e8770698c6ed770ff2f6d14e52e3010
Liza Daneris Medina <lizzmedina15@gmail.com>
2023-03-06 22:38:14 -0500

    se creó la etiqueta formulario en el componente app

--- primerParcialFrontIII/src/app.jsx
+++ primerParcialFrontIII/src/app.jsx
@@ -5,7 +5,10 @@ import Card from "./Card";
 function App() {
   return (
     <div className="App">
-    <Card />
+      <h1>Carga de estudiantes</h1>
+      <form> 
+      </form>
+      <Card />
   </div>
   );
 }
