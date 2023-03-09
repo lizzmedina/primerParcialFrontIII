b0a3940b67847a9dad62897c8a60732d6f09dd47
Liza Daneris Medina <lizzmedina15@gmail.com>
2023-03-08 22:57:22 -0500

    estilos con bootstrap

--- primerParcialFrontIII/src/app.jsx
+++ primerParcialFrontIII/src/app.jsx
@@ -1,46 +1,66 @@
-import {useState} from 'react';
-import './app.css';
-import Card from './components/Card';
-
+import { useState } from "react";
+import "./app.css";
+import Card from "./components/Card";
 
 function App() {
-
   const [info, setInfo] = useState({
-    nombre: '',
-    dato: '',
-  })
+    nombre: "",
+    dato: "",
+  });
   const [show, setShow] = useState(false);
   const [err, setErr] = useState(false);
 
   const handlerSubmit = (event) => {
-      event.preventDefault();
-      if (info.nombre.length > 3 && info.dato.length > 6 && info.nombre[0] !== " ") {
-        setShow(true)
-        setErr(false)
-      } else {
-        setErr(true)
-  }
-}
+    event.preventDefault();
+    if (
+      info.nombre.length > 3 &&
+      info.dato.length > 6 &&
+      info.nombre[0] !== " "
+    ) {
+      setShow(true);
+      setErr(false);
+    } else {
+      setErr(true);
+    }
+  };
   return (
-    <div className="app">
-      <p> ¿ Cuál es tu libro favorito ? </p>
-      <form onSubmit={handlerSubmit}> 
-      <div className='container-form'>
-        <input className='nombre-input' type="text" value={info.nombre} onChange={(e)=> setInfo ({...info, nombre: e.target.value})} placeholder='ingresa tú nombre'/>        
-        <hr />
-        <input className='dato-input' type="text"  value={info.dato} onChange={(e)=> setInfo ({...info, dato: e.target.value})} placeholder='ingresa tú respuesta' />
-        <hr />
-        <button type="submit">Agregar</button>
+    <div className="card">
+      <div class="card-header text-center h4">
+        {" "}
+        ¿ Cuál es tu libro favorito ?{" "}
       </div>
-        
+      <form onSubmit={handlerSubmit}>
+        <div className="card-body">
+          <input
+            className="nombre-input form-control"
+            type="text"
+            value={info.nombre}
+            onChange={(e) => setInfo({ ...info, nombre: e.target.value })}
+            placeholder="ingresa tú nombre"
+          />
+          <hr />
+          <input
+            className="dato-input form-control"
+            type="text"
+            value={info.dato}
+            onChange={(e) => setInfo({ ...info, dato: e.target.value })}
+            placeholder="ingresa tú respuesta"
+          />
+          <hr />
+          <div class="d-grid gap-2">
+            <button className="btn btn-primary" type="submit">
+              {" "}
+              Agregar{" "}
+            </button>
+          </div>
+        </div>
       </form>
-      <div className='error-container'>
-        {err && 'Por favor chequea que la información sea correcta'}
+      <div className="error-container">
+        {err && "Por favor chequea que la información sea correcta"}
       </div>
-      
-        {show && <Card nombre={info.nombre} dato={info.dato}/>}
-      
-  </div>
+
+      {show && <Card nombre={info.nombre} dato={info.dato} />}
+    </div>
   );
 }
 export default App;
--- primerParcialFrontIII/src/components/Card.jsx
+++ primerParcialFrontIII/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import '../App.css'
 export const Card = ({nombre, dato}) => {
     return (
-        <div className='container-card'>
+        <div className='container bg-info'>
             <h3> ¡ Hola {nombre} !</h3>
             <h3>Ahora sabemos que tú libro favorito es: {dato}</h3>
         </div>
