205fc032ce1ec5b906d3fe25360fa58279b29494
Liza Daneris Medina <lizzmedina15@gmail.com>
2023-03-08 21:11:34 -0500

    completado el parcial con estilos basicos

--- primerParcialFrontIII/src/app.jsx
+++ primerParcialFrontIII/src/app.jsx
@@ -1,14 +1,40 @@
-
+import {useState} from 'react';
 import './app.css';
-import Card from "./Card";
+import Card from './components/Card';
+
 
 function App() {
+
+  const [info, setInfo] = useState({
+    nombre: '',
+    dato: '',
+  })
+  const [show, setShow] = useState(false);
+  const [err, setErr] = useState(false);
+
+  const handlerSubmit = (event) => {
+      event.preventDefault();
+      if (info.nombre.length > 3 && info.dato.length > 6 && info.nombre[0] !== " ") {
+        setShow(true)
+        setErr(false)
+      } else {
+        setErr(true)
+  }
+}
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form> 
+      <h1> ¿ Cuál es tu libro favorito ? </h1>
+      <form onSubmit={handlerSubmit}> 
+        <input className='nombre-input' type="text" value={info.nombre} onChange={(e)=> setInfo ({...info, nombre: e.target.value})} placeholder='ingresa tú nombre'/>        
+        <hr />
+        <input className='dato-input' type="text"  value={info.dato} onChange={(e)=> setInfo ({...info, dato: e.target.value})} placeholder='ingresa tú respuesta' />
+        <hr />
+        <button type="submit">Agregar</button>
       </form>
-      <Card />
+      <div className='error-container'>
+        {err && 'Por favor chequea que la información sea correcta'}
+      </div>
+      {show && <Card nombre={info.nombre} dato={info.dato}/>}
   </div>
   );
 }
--- primerParcialFrontIII/src/components/Card.jsx
+++ primerParcialFrontIII/src/components/Card.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-
-const Card = () => {
-
-    return <div>Hola, soy una tarjeta</div>;
-};
-
+import '../App.css'
+export const Card = ({nombre, dato}) => {
+    return (
+        <div className='container-card'>
+            <h3> ¡ Hola {nombre} !</h3>
+            <h3>Ahora sabemos que tú libro favorito es: {dato}</h3>
+        </div>
+    )
+}
 export default Card;
